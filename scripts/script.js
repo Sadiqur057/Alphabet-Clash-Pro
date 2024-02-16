95c6e87bed0a16704707c7c2e5298797cdc32c5e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-02-16 23:43:28 +0600

    added all functionality to play the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,67 @@
+function handleKeyboardKeyPress(event) {
+  const playerPressed = event.key;
+
+  // if player pressed escape then game will be over
+  if (playerPressed === "Escape") {
+    gameOver();
+  }
+  const currentAlphabet = getElementTextById("current-alphabet");
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  let currentScore = getElementTextValueById("current-score");
+  
+  // if player pressed correct one then score will increase;
+  if (playerPressed === expectedAlphabet) {
+    currentScore += 1;
+    setTextElementById("current-score", currentScore);
+
+    // continue to next round
+    removeBackgroundColorById(playerPressed);
+    continueGame();
+
+    // if player choose wrong answer then life will be deducted
+  } else {
+    console.log(currentScore);
+    console.log("wrong");
+    let currentLife = getElementTextValueById("current-life");
+    currentLife -= 1;
+    setTextElementById("current-life", currentLife);
+
+    // game over when player has no life
+    if (currentLife === 0) {
+      gameOver();
+    }
+  }
+}
+document.addEventListener("keyup", handleKeyboardKeyPress);
+
+function gameOver() {
+  hideElementsById("play-ground-screen");
+  showElementsById("score-board-screen");
+
+  // show the final score
+  const finalScore = getElementTextById("current-score");
+  setTextElementById("final-score", finalScore);
+
+  // clear the highlight of the last selected text on keyboard
+  const currentAlphabet = getElementTextById("current-alphabet");
+  removeBackgroundColorById(currentAlphabet);
+}
+
 function continueGame() {
   // get random alphabet
   const alphabet = getRandomAlphabet();
-  setElementById("current-alphabet", alphabet);
-  console.log(alphabet);
+  setTextElementById("current-alphabet", alphabet);
+  setBackgroundColorById(alphabet);
 }
 
 function play() {
   hideElementsById("home-screen");
+  hideElementsById("score-board-screen");
   showElementsById("play-ground-screen");
+
+  // reset score
+  setTextElementById("current-life", 5);
+  setTextElementById("current-score", 0);
+
   continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,23 +1,47 @@
-function hideElementsById(elementId){
+function hideElementsById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add("hidden");
 }
 
-function showElementsById(elementId){
+function showElementsById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
 
-function getRandomAlphabet(){
-  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetArray = alphabets.split('');
-  const randomNumber = Math.random()*25;
+function getRandomAlphabet() {
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArray = alphabets.split("");
+  const randomNumber = Math.random() * 25;
   const index = Math.round(randomNumber);
   const alphabet = alphabets[index];
   return alphabet;
 }
 
-function setElementById(elementId,value){
+
+function setTextElementById(elementId, value) {
   const element = document.getElementById(elementId);
   element.innerText = value;
+}
+
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
+}
+
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+}
+
+function getElementTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerText;
+  return elementText;
+}
+
+function getElementTextValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerText;
+  const elementTextValue = parseInt(elementText);
+  return elementTextValue;
 }
